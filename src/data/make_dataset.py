583c263aaa76bc7e852a656e4a375f5230d5c069
caheredia <cristianheredia@gmail.com>
2019-03-28 01:04:03 -0700

    remove requirements from make data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-import click
+import os 
+import pandas as pd 
+import numpy as np 
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -15,16 +12,20 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    TITANIC_PATH = os.path.join("data", "raw")
+
+    def load_data(filename, titanic_path=TITANIC_PATH):
+        csv_path = os.path.join(titanic_path, filename)
+        return pd.read_csv(csv_path)
+
+    train_data = load_data("train.csv")
+
+    print(train_data.head())
+  
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    logging.basicConfig(level=logging.DEBUG, format=log_fmt)
 
     main()
