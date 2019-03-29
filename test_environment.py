235af4cd7ef98224d950457a2eb746392e5cab8d
caheredia <cristianheredia@gmail.com>
2019-03-29 01:10:42 -0700

    write test for python version

--- test_environment.py
+++ test_environment.py
@@ -1,25 +1,8 @@
 import sys
+import pytest
 
-REQUIRED_PYTHON = "python3"
-
-
-def main():
-    system_major = sys.version_info.major
-    if REQUIRED_PYTHON == "python":
-        required_major = 2
-    elif REQUIRED_PYTHON == "python3":
-        required_major = 3
-    else:
-        raise ValueError("Unrecognized python interpreter: {}".format(
-            REQUIRED_PYTHON))
-
-    if system_major != required_major:
-        raise TypeError(
-            "This project requires Python {}. Found: Python {}".format(
-                required_major, sys.version))
-    else:
-        print(">>> Development environment passes all tests!")
+PYTHON_VERSION = 3
 
 
-if __name__ == '__main__':
-    main()
+def test_version():
+    assert PYTHON_VERSION == sys.version_info.major
