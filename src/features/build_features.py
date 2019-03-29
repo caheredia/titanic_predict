3d7de4a5711ee86908653eb91df93b57f9702062
caheredia <cristianheredia@gmail.com>
2019-03-29 14:49:19 -0700

    add relatives function

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -18,3 +18,26 @@ def load_data(filename, titanic_path=TITANIC_PATH):
     csv_path = os.path.join(titanic_path, filename)
     logger.debug('Loading csv into pandas dataframe')
     return pd.read_csv(csv_path)
+
+
+def add_num_features(df, column_names, relatives=True):
+    '''Adds extra features to data sets.
+
+    If boolean is true adds relatives on board.
+
+    Parameters
+    ----------
+    df : pandas.core.frame.DataFrame
+        dataframe to modify
+    column_names : list
+        columns containing relative data
+
+    Returns
+    -------
+    df : numpy.Array
+        transformed df .
+    '''
+    logger.debug('Adding RelativesOnboard column')
+    if relatives:
+        df['RelativeOnboard'] = df[column_names].sum(axis=1)
+        return df
