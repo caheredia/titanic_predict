c9bf4656f739232714a5a79e5ce5b1501409d4dc
caheredia <cristianheredia@gmail.com>
2019-03-29 01:30:47 -0700

    create test for data files

--- test_environment.py
+++ test_environment.py
@@ -1,8 +1,18 @@
 import sys
 import pytest
+import os
 
 PYTHON_VERSION = 3
+TITANIC_PATH = os.path.join("data", "raw")
+DATA_FILES = ['train.csv', 'test.csv']
 
 
-def test_version():
+def test_python_version():
+    """Test for python 3 major version"""
     assert PYTHON_VERSION == sys.version_info.major
+
+
+def test_raw_data():
+    for file in DATA_FILES:
+        filename = os.path.join(TITANIC_PATH, file)
+        assert os.path.exists(filename)
